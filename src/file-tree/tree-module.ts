da602524f2ed1575532f0aaf96ec2c87099ffce3
James Chen <jameschan312.cn@gmail.com>
2025-08-18 21:08:49 -0400

    Added comment for tree module

--- src/file-tree/tree-module.ts
+++ src/file-tree/tree-module.ts
@@ -9,20 +9,38 @@ import type { RawNeoFileTree } from './types.js'
  * name.
  */
 export class TreeModule {
-  readonly trees: Record<string, NeoFileTree> = {}
+  /**
+   * A mapping of tree names to their corresponding NeoFileTree instances.
+   */
+  readonly treeMap: Record<string, NeoFileTree> = {}
 
+  /**
+   * Creates a new NeoFileTree from raw data and stores it in the treeMap.
+   *
+   * @param name - The name to associate with the new file tree.
+   * @param rawFileTree - The raw data to convert into a NeoFileTree.
+   * @returns The newly created NeoFileTree instance.
+   */
   create(name: string, rawFileTree: RawNeoFileTree): NeoFileTree {
-    if (this.trees[name]) {
+    if (this.treeMap[name]) {
       throw new Error(`File tree with name "${name}" already exists.`)
     }
 
-    this.trees[name] = NeoFileTreeConverter.fromRawFileTree(rawFileTree)
+    this.treeMap[name] = NeoFileTreeConverter.fromRawFileTree(rawFileTree)
 
-    return this.trees[name]
+    return this.treeMap[name]
   }
 
+  /**
+   * Gets an existing NeoFileTree by name.
+   *
+   * @param name - The name of the file tree to retrieve.
+   * @returns The NeoFileTree instance associated with the given name.
+   *
+   * @throws ReferenceError if no file tree with the given name exists.
+   */
   get(name: string): NeoFileTree {
-    const fileTree = this.trees[name]
+    const fileTree = this.treeMap[name]
     if (!fileTree) {
       throw new ReferenceError(`File tree with name "${name}" does not exist.`)
     }
